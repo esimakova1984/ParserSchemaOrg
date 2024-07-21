05cdd53323916a68e32a3874e59242ee78ad180e
Ekaterina Simakova <ekaterinasimakova@Ekaterinas-MacBook-Pro.local>
2024-07-21 14:55:56 +0300

    update

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-
-# from schema_comparer import SchemaComparer
-# from url_parser import UrlTemplate
-#
-# headers = {
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-# }
-#
-# url_compare = UrlTemplate(
-#     "https://www.shopper.shop/store/medika-pharmacy/product/%d7%97%d7%99%d7%9c%d7%91%d7%94-%d7%90%d7%9c%d7%98%d7%9e%d7%9f-2"
-# )
-#
-# schema_comparer = SchemaComparer(url_compare, headers=headers)
-# schema_comparer.compare_schemas()
-
 from schema_comparer import SchemaComparer
 
 headers = {
--- url_parser.py
+++ url_parser.py
@@ -1,59 +1,8 @@
-# import json
-# import re
-# import requests
-# from bs4 import BeautifulSoup
-#
-#
-# class UrlTemplate:
-#     def __init__(self, url, headers=None):
-#         self.url = url
-#         self.headers = headers or {}
-#         self.metadata = None
-#
-#     def get_metadata(self):
-#         response = requests.get(self.url, headers=self.headers)
-#         soup = BeautifulSoup(response.text, 'html.parser')
-#         schemaorg_elements = soup.find_all('script', {'type': 'application/ld+json'})
-#
-#         # Create an empty list to store product metadata
-#         metadata = []
-#
-#         # Iterate over elements containing scripts with schema.org metadata
-#         for element in schemaorg_elements:
-#             # Clean the text of the element from control characters
-#             cleaned_text = self.clean_control_characters(element.text)
-#
-#             # Parse the cleaned text into a JSON object
-#             json_data = json.loads(cleaned_text)
-#
-#             # Check for the presence of the "@type" key and its value being "Product"
-#             if "@type" in json_data and json_data["@type"] == "Product":
-#                 # If the condition is met, add the metadata to the list
-#                 metadata.append(json_data)
-#
-#         if len(metadata) > 1:
-#             raise ValueError("Error: More than one script with '@type': 'Product' found. Please ensure only one is present.")
-#
-#         self.metadata = metadata[0] if metadata else None
-#
-#     def clean_control_characters(self, text):
-#         return re.sub(r'[\x00-\x1F\x7F-\x9F]', '', text)
-#
-#     def check_required_keys(self):
-#         if self.metadata is None:
-#             print(f"Error: Metadata does not exist for {self.url}.")
-#             return
-#
-#         required_keys = ["@context", "@type", "name", "image", "description", "sku", "brand", "offers", "aggregateRating"]
-#
-#         for key in required_keys:
-#             if key not in self.metadata:
-#                 print(f"Error: Required key '{key}' is missing in metadata for {self.url}.")
-
 import json
 import re
 import requests
 from bs4 import BeautifulSoup
+
 
 class UrlTemplate:
     def __init__(self, url, headers=None):
@@ -83,7 +32,8 @@ class UrlTemplate:
                 metadata.append(json_data)
 
         if len(metadata) > 1:
-            raise ValueError("Error: More than one script with '@type': 'Product' found. Please ensure only one is present.")
+            raise ValueError(
+                "Error: More than one script with '@type': 'Product' found. Please ensure only one is present.")
 
         self.metadata = metadata[0] if metadata else None
 
